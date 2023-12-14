2c65b1890c03add4297b895f1c4aaf1c708da413
janithrs <janithravindu@gmail.com>
2023-12-14 07:53:58 +0530

    remove unwanted files

---
+++ nehr-client-app/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"lJV1ugN0HQcPyMGW/4s1y81N5loRD7ArKEKjfECi1NI=\"\n}"
--- nehr-next/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"594P5w4hAS5/1QtedNqGLiSwV0rbCWtHyuYDZa/keSM=\"\n}"

dd2a4c548a074743812bfd7ab574d85e000b2c0c
janithrs <janithravindu@gmail.com>
2023-12-15 21:24:12 +0530

    initialize localization

--- nehr-patient-portal/app/[locale]/page.tsx
+++ nehr-patient-portal/app/[locale]/page.tsx
@@ -2,7 +2,7 @@ import { Footer } from "@/modules/footer";
 import { HeroSection } from "@/modules/home-hero";
 import { NavBar } from "@/modules/nav-bar";
 
-export default function Home() {
+const Home = () => {
   return (
     <div className="min-h-screen">
       <NavBar />
@@ -10,4 +10,6 @@ export default function Home() {
       <Footer />
     </div>
   );
-}
+};
+
+export default Home;

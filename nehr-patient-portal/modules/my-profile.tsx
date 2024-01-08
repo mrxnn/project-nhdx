57f17c014ca65f1652a63c7ca03bfa8cf60db7b7
Mayura <mayura.ramanayake@gmail.com>
2024-01-08 13:44:30 +0530

    Add correct labels and placeholders

--- nehr-patient-portal/modules/my-profile.tsx
+++ nehr-patient-portal/modules/my-profile.tsx
@@ -12,34 +12,34 @@ export const MyProfile = () => {
           <input
             type="text"
             name="name"
-            placeholder="Janith Silva"
+            placeholder="Name"
             className="px-3 py-2 border border-slate-200 rounded-md"
           />
         </div>
         <div className="flex flex-col text-sm gap-y-1.5">
-          <label htmlFor="name">Name</label>
+          <label htmlFor="gender">Gender</label>
           <input
             type="text"
-            name="name"
-            placeholder="Janith Silva"
+            name="gender"
+            placeholder="Gender"
             className="px-3 py-2 border border-slate-200 rounded-md"
           />
         </div>
         <div className="flex flex-col text-sm gap-y-1.5">
-          <label htmlFor="name">Name</label>
+          <label htmlFor="date-of-birth">Date of Birth</label>
           <input
             type="text"
-            name="name"
-            placeholder="Janith Silva"
+            name="date-of-birth"
+            placeholder="Date of Birth"
             className="px-3 py-2 border border-slate-200 rounded-md"
           />
         </div>
         <div className="flex flex-col text-sm gap-y-1.5">
-          <label htmlFor="name">Name</label>
+          <label htmlFor="marital-status">Marital Status</label>
           <input
             type="text"
-            name="name"
-            placeholder="Janith Silva"
+            name="marital-status"
+            placeholder="Marital Status"
             className="px-3 py-2 border border-slate-200 rounded-md"
           />
         </div>
@@ -49,38 +49,38 @@ export const MyProfile = () => {
       <Heading>Identifiers</Heading>
       <div className="grid md:grid-cols-3 gap-5">
         <div className="flex flex-col text-sm gap-y-1.5">
-          <label htmlFor="name">Name</label>
+          <label htmlFor="national-id-number">National ID Number</label>
           <input
             type="text"
-            name="name"
-            placeholder="Janith Silva"
+            name="national-id-number"
+            placeholder="NIC"
             className="px-3 py-2 border border-slate-200 rounded-md"
           />
         </div>
         <div className="flex flex-col text-sm gap-y-1.5">
-          <label htmlFor="name">Name</label>
+          <label htmlFor="passport-number">Passport Number</label>
           <input
             type="text"
-            name="name"
-            placeholder="Janith Silva"
+            name="passport number"
+            placeholder="Passport Number"
             className="px-3 py-2 border border-slate-200 rounded-md"
           />
         </div>
         <div className="flex flex-col text-sm gap-y-1.5">
-          <label htmlFor="name">Name</label>
+          <label htmlFor="driving-license-number">Driving License Number</label>
           <input
             type="text"
-            name="name"
-            placeholder="Janith Silva"
+            name="driving-license-number"
+            placeholder="Driving License Number"
             className="px-3 py-2 border border-slate-200 rounded-md"
           />
         </div>
         <div className="flex flex-col text-sm gap-y-1.5">
-          <label htmlFor="name">Name</label>
+          <label htmlFor="senior-cityzen-number">Senior Citizen Number</label>
           <input
             type="text"
-            name="name"
-            placeholder="Janith Silva"
+            name="senior-cityzen-number"
+            placeholder="Senior Citizen Number"
             className="px-3 py-2 border border-slate-200 rounded-md"
           />
         </div>
@@ -95,24 +95,70 @@ export const MyProfile = () => {
         </label>
       </div>
 
-      {/* Other */}
-      <Heading>Other</Heading>
-      <div className="space-y-5 md:max-w-[calc((100%-40px)/3)]">
+      {/* Contact Info */}
+      <Heading>Contact Information</Heading>
+      <div className="grid md:grid-cols-3 gap-5">
         <div className="flex flex-col text-sm gap-y-1.5">
-          <label htmlFor="name">Name</label>
+          <label htmlFor="mobile-number">Mobile Number</label>
           <input
             type="text"
-            name="name"
-            placeholder="Janith Silva"
+            name="mobile-number"
+            placeholder="Mobile Number"
             className="px-3 py-2 border border-slate-200 rounded-md"
           />
         </div>
         <div className="flex flex-col text-sm gap-y-1.5">
-          <label htmlFor="name">Name</label>
+          <label htmlFor="email">Email Address</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email Address"
+            className="px-3 py-2 border border-slate-200 rounded-md"
+          />
+        </div>
+        <div>&nbsp;</div>
+        <div className="flex flex-col text-sm gap-y-1.5 md:col-span-2">
+          <label htmlFor="address">Address</label>
           <input
             type="text"
-            name="name"
-            placeholder="Janith Silva"
+            name="address"
+            placeholder="Address"
+            className="px-3 py-2 border border-slate-200 rounded-md"
+          />
+        </div>
+        <div className="flex flex-col text-sm gap-y-1.5">
+          <label htmlFor="grama-niladari-division">
+            Grama Niladari Division
+          </label>
+          <input
+            type="text"
+            name="grama-niladari-division"
+            placeholder="Grama Niladari Division"
+            className="px-3 py-2 border border-slate-200 rounded-md"
+          />
+        </div>
+      </div>
+
+      {/* Other */}
+      <Heading>Other</Heading>
+      <div className="space-y-5 md:max-w-[calc((100%-40px)/3)]">
+        <div className="flex flex-col text-sm gap-y-1.5">
+          <label htmlFor="occupation">Occupation</label>
+          <input
+            type="text"
+            name="occupation"
+            placeholder="Occupation"
+            className="px-3 py-2 border border-slate-200 rounded-md"
+          />
+        </div>
+        <div className="flex flex-col text-sm gap-y-1.5">
+          <label htmlFor="highest-education-level">
+            Highest Education Level
+          </label>
+          <input
+            type="text"
+            name="highest-education-level"
+            placeholder="Highest Education Level"
             className="px-3 py-2 border border-slate-200 rounded-md"
           />
         </div>
@@ -122,38 +168,38 @@ export const MyProfile = () => {
       <Heading>Guardian Information</Heading>
       <div className="grid md:grid-cols-3 gap-5">
         <div className="flex flex-col text-sm gap-y-1.5">
-          <label htmlFor="name">Name</label>
+          <label htmlFor="guardian-name">Name</label>
           <input
             type="text"
-            name="name"
-            placeholder="Janith Silva"
+            name="guardian-name"
+            placeholder="Name"
             className="px-3 py-2 border border-slate-200 rounded-md"
           />
         </div>
         <div className="flex flex-col text-sm gap-y-1.5">
-          <label htmlFor="name">Name</label>
+          <label htmlFor="guardian-address">Address</label>
           <input
             type="text"
-            name="name"
-            placeholder="Janith Silva"
+            name="guardian-address"
+            placeholder="Address"
             className="px-3 py-2 border border-slate-200 rounded-md"
           />
         </div>
         <div className="flex flex-col text-sm gap-y-1.5">
-          <label htmlFor="name">Name</label>
+          <label htmlFor="guardian-mobile-number">Telephone Number</label>
           <input
             type="text"
-            name="name"
-            placeholder="Janith Silva"
+            name="guardian-mobile-number"
+            placeholder="Telephone Number"
             className="px-3 py-2 border border-slate-200 rounded-md"
           />
         </div>
         <div className="flex flex-col text-sm gap-y-1.5">
-          <label htmlFor="name">Name</label>
+          <label htmlFor="relationship">Relationship</label>
           <input
             type="text"
-            name="name"
-            placeholder="Janith Silva"
+            name="relationship"
+            placeholder="Relationship"
             className="px-3 py-2 border border-slate-200 rounded-md"
           />
         </div>

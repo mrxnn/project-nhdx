d022e7f8fb2711b1df1a510bacfcec07bd925dc1
Mayura <mayura.ramanayake@gmail.com>
2024-03-03 21:02:55 +0530

    generate barcode for the PHN

--- nehr-patient-portal/app/[locale]/onboarding/steps/phn-card.tsx
+++ nehr-patient-portal/app/[locale]/onboarding/steps/phn-card.tsx
@@ -1,21 +1,28 @@
+"use client";
+
 import Link from "next/link";
+import Barcode from "react-barcode";
 import { Button } from "@/components/ui/button";
 import { ChevronRight } from "lucide-react";
+import { useGlobalStore } from "@/lib/store";
 
 export const PHNCard = () => {
+  const { phn } = useGlobalStore((state) => state.onboardingFormData);
+
   return (
     <div className="divide-y">
       <div className="pb-12">
         <p className="bg-slate-900 inline-block px-3 py-px rounded-lg mb-2">
-          <span className="text-slate-100 font-bold">#123456</span>
+          <span className="text-slate-100 font-bold">#{phn}</span>
         </p>
-        <p className="font-bold text-3xl tracking-tight mb-2">
-          Your PHN is generated
-        </p>
-        <p className="text-slate-500 mb-8">
+        <p className="font-bold text-3xl tracking-tight mb-2">Your PHN Card</p>
+        <p className="text-slate-500">
           Explain what the PHN is and how it is used for, and some info about
           the artwork.
         </p>
+        <div className="-translate-x-[11px] py-4">
+          <Barcode value={`PHN ---- #${phn}`} textAlign="right" fontSize={14} />
+        </div>
         <Button className="space-x-2" asChild>
           <Link href="/dashboard">
             <span>Go to dashboard</span>

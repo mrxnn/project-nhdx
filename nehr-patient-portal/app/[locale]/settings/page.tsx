ab226b86562b9ac9a298e5ae7dfaa8d89ad67db7
Mayura <mayura.ramanayake@gmail.com>
2024-03-06 04:40:16 +0530

    Basic layout for settings

---
+++ nehr-patient-portal/app/[locale]/settings/page.tsx
@@ -0,0 +1,78 @@
+import Link from "next/link";
+import { PageParams } from "@/lib/types";
+import { ReactNode } from "react";
+import { cn } from "@/lib/utils";
+
+const SectionLink = ({
+  children,
+  href,
+  isActive,
+}: {
+  children: ReactNode;
+  href: string;
+  isActive: boolean;
+}) => {
+  return (
+    <Link
+      className={cn(
+        "transition-all",
+        isActive
+          ? "text-blue-500 hover:text-blue-500 scale-100"
+          : "text-slate-50 hover:text-slate-300 scale-95"
+      )}
+      href={`?section=${href}`}>
+      {children}
+    </Link>
+  );
+};
+
+export default function SettingsPage(props: PageParams) {
+  if (!props.searchParams.section) throw new Error("Missing param: `section`");
+  if (Array.isArray(props.searchParams.section))
+    throw new Error("Invalid param: `section`");
+
+  const { section } = props.searchParams;
+
+  return (
+    <>
+      <div className="flex min-h-screen">
+        <div className="flex-1 bg-slate-900 text-slate-50 flex flex-col justify-center items-end pr-10 gap-y-2">
+          <SectionLink isActive={section === "my-phn"} href="my-phn">
+            My PHN
+          </SectionLink>
+          <SectionLink
+            isActive={section === "demographics"}
+            href="demographics">
+            Demographics
+          </SectionLink>
+          <SectionLink isActive={section === "identifiers"} href="identifiers">
+            Personal Identifiers
+          </SectionLink>
+          <SectionLink
+            isActive={section === "contact-details"}
+            href="contact-details">
+            Contact Info
+          </SectionLink>
+          <SectionLink
+            isActive={section === "work-and-education"}
+            href="work-and-education">
+            Work & Education
+          </SectionLink>
+          <SectionLink
+            isActive={section === "emergency-contact"}
+            href="emergency-contact">
+            Emergency Contact
+          </SectionLink>
+          <SectionLink
+            isActive={section === "dependent-profiles"}
+            href="dependent-profiles">
+            Dependent Profiles
+          </SectionLink>
+        </div>
+        <div className="flex-1 flex justify-center items-center">
+          <p>{props.searchParams.section}</p>
+        </div>
+      </div>
+    </>
+  );
+}

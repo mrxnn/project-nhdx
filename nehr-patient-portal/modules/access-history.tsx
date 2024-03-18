6f8e86fe9213eadc51d1a34ec1db6745e9e41474
Mayura <mayura.ramanayake@gmail.com>
2024-03-18 19:21:28 +0530

    Create UIs for access history and privacy pages

--- nehr-patient-portal/modules/access-history.tsx
+++ nehr-patient-portal/modules/access-history.tsx
@@ -1,7 +1,100 @@
+import { DataTable } from "@/components/data-table";
+import { Button } from "@/components/ui/button";
+import { ColumnDef } from "@tanstack/react-table";
+import { FAQ } from "./faq";
+
 export const AccessHistory = () => {
   return (
     <div>
-      <h1>Access History</h1>
+      <h1 className="text-xl font-bold mb-5">Portal Access History</h1>
+      <DataTable columns={columns} data={history} />
+      <ArchivedRecords count={3} />
+
+      <h1 className="text-xl font-bold mb-5 mt-16">Provider Access History</h1>
+      <DataTable columns={columns} data={history} />
+      <ArchivedRecords count={8} />
+
+      <FAQ className="mt-12" />
     </div>
   );
 };
+
+const ArchivedRecords = ({ count }: { count: number }) => {
+  return (
+    <div className="flex items-center gap-x-2 mt-4">
+      <div className="size-8 rounded-full text-white bg-slate-950 flex justify-center items-center">
+        <span>{count}</span>
+      </div>
+      <span className="text-sm font-medium">Archived Records</span>
+    </div>
+  );
+};
+
+export type History = {
+  phn: string;
+  datetime: string;
+  duration: string;
+  institution: string;
+  provider: string;
+};
+
+export const columns: ColumnDef<History>[] = [
+  {
+    accessorKey: "phn",
+    header: "PHN",
+    cell: ({ row }) => {
+      const phn = row.getValue("phn");
+      return `#${phn}`;
+    },
+  },
+  {
+    accessorKey: "datetime",
+    header: "DATE & TIME",
+  },
+  {
+    accessorKey: "duration",
+    header: "DURATION",
+  },
+  {
+    accessorKey: "institution",
+    header: "INSTITUTION",
+  },
+  {
+    accessorKey: "provider",
+    header: "PROVIDER",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      return (
+        <Button className="w-[90px]" size="sm" variant="destructive">
+          Archive
+        </Button>
+      );
+    },
+  },
+];
+
+export const history: History[] = [
+  {
+    phn: "334422",
+    datetime: "2022-12-19",
+    duration: "20 Mins",
+    institution: "National hospital of Sri Lanka",
+    provider: "Google",
+  },
+  {
+    phn: "334422",
+    datetime: "2022-12-21",
+    duration: "10 Mins",
+    institution: "National hospital of Sri Lanka",
+    provider: "Facebook",
+  },
+  {
+    phn: "334422",
+    datetime: "2022-12-19",
+    duration: "20 Mins",
+    institution: "National hospital of Sri Lanka",
+    provider: "Google",
+  },
+];
--- nehr-patient-portal/modules/privacy-and-access.tsx
+++ nehr-patient-portal/modules/privacy-and-access.tsx
@@ -1,7 +1,102 @@
+import { DataTable } from "@/components/data-table";
+import { Button } from "@/components/ui/button";
+import { ColumnDef } from "@tanstack/react-table";
+import { FAQ } from "./faq";
+import { Input } from "@/components/ui/input";
+
 export const PrivacyAndAccess = () => {
   return (
     <div>
-      <h1>Privacy and Access</h1>
+      <div className="flex justify-between items-center mb-5">
+        <h1 className="text-xl font-bold">Institutions with Read Access</h1>
+        <Input className="w-56" placeholder="Search" />
+      </div>
+      <DataTable
+        columns={columns}
+        data={institutions.filter((i) => i.status === "granted")}
+      />
+
+      <div className="flex justify-between items-center mb-5 mt-16">
+        <h1 className="text-xl font-bold">Institutions with Revoked Access</h1>
+        <Input className="w-56" placeholder="Search" />
+      </div>
+      <DataTable
+        columns={columns}
+        data={institutions.filter((i) => i.status === "revoked")}
+      />
+
+      <FAQ className="mt-12" />
     </div>
   );
 };
+
+export type Institution = {
+  datetime: string;
+  institution: string;
+  status: "granted" | "revoked";
+};
+
+export const columns: ColumnDef<Institution>[] = [
+  {
+    accessorKey: "datetime",
+    header: "DATE & TIME",
+  },
+  {
+    accessorKey: "institution",
+    header: "INSTITUTION",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const status = row.original.status;
+      if (status === "granted") {
+        return (
+          <Button className="w-[90px]" size="sm" variant="destructive">
+            Revoke
+          </Button>
+        );
+      }
+
+      if (status === "revoked") {
+        return (
+          <Button className="w-[90px]" size="sm">
+            Reinstate
+          </Button>
+        );
+      }
+    },
+  },
+];
+
+export const institutions: Institution[] = [
+  {
+    datetime: "2022-12-19",
+    institution: "National hospital of Sri Lanka",
+    status: "granted",
+  },
+  {
+    datetime: "2022-12-19",
+    institution: "National hospital of Sri Lanka",
+    status: "granted",
+  },
+  {
+    datetime: "2022-12-19",
+    institution: "National hospital of Sri Lanka",
+    status: "granted",
+  },
+  {
+    datetime: "2022-12-19",
+    institution: "National hospital of Sri Lanka",
+    status: "revoked",
+  },
+  {
+    datetime: "2022-12-19",
+    institution: "National hospital of Sri Lanka",
+    status: "revoked",
+  },
+  {
+    datetime: "2022-12-19",
+    institution: "National hospital of Sri Lanka",
+    status: "revoked",
+  },
+];
